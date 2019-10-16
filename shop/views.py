6a43469b32d501e12c99d93e49fc6d8a6b441959
Ahmad <x00148588@mytudublin.ie>
2019-10-16 12:50:37 +0100

    done

---
+++ shop/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import ListView
+from .models import Product, Category
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'home.html'
+    context_object_name = 'all_products_list'
+
+    def categories(self):
+        return Category.objects.all()
+# Create your views here.
